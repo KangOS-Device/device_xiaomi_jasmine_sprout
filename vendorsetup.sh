24603056faa6616341327be50e8bef1b0353539d
nparashar150 <nparashar150@gmail.com>
2021-04-23 09:53:22 +0530

    Clone AshutoshSundresh's vendor/gapps     Remove vendor/gapps from source

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,3 +3,5 @@ export SKIP_ABI_CHECKS=true
 export BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
 rm -rf device/xiaomi/sdm660-common/DeviceDoze
 rm -rf device/xiaomi/sdm660-common/DeviceSettings
+rm -rf vendor/gapps
+git clone https://gitlab.com/AshutoshSundresh/vendor_gapps.git vendor/gapps
