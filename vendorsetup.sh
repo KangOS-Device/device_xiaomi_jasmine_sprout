64859cb6d8dcc2bad4e27638a43843076da8bfa5
nparashar150 <nparashar150@gmail.com>
2021-04-23 09:50:59 +0530

    SKIP_ABI_CHECKS

---
+++ vendorsetup.sh
@@ -0,0 +1,12 @@
+# export
+export SKIP_ABI_CHECKS=true
+export BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
+rm -rf device/xiaomi/sdm660-common/DeviceDoze
+rm -rf device/xiaomi/sdm660-common/DeviceSettings
+rm -rf vendor/gapps
+git clone https://gitlab.com/AshutoshSundresh/vendor_gapps.git vendor/gapps
+rm -rf packages/resources/devicesettings
+git clone https://github.com/LineageOS/android_packages_resources_devicesettings.git -b lineage-18.1 packages/resources/devicesettings
+rm -rf vendor/codeaurora/telephony
+git clone https://github.com/OdSazib/android_vendor_codeaurora_telephony.git vendor/codeaurora/telephony/
+
