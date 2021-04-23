6202164eae258b5814ed03d1778da254c45e3e85
nparashar150 <nparashar150@gmail.com>
2021-04-23 09:49:36 +0530

    jasmine_sprout: Bringup Kang OS

--- kangos_jasmine_sprout.mk
+++ kangos_jasmine_sprout.mk
@@ -1,6 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
-# Copyright (C) 2018-2021 Xiaomi-SDM660 Project
+# Copyright (C) 2018 The Xiaomi-SDM660 Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,13 +13,24 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+PRODUCT_PRODUCT_PROPERTIES += \
+  ro.kangos.maintainer=nparashar150 \
+  ro.kangos.cpu=SDM660
+
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/jasmine_sprout/device.mk)
 
 # Inherit some common stuff from Project-Awaken
-AWAKEN_BUILD_TYPE := OFFICIAL
-$(call inherit-product, vendor/awaken/config/common.mk)
-$(call inherit-product, vendor/awaken/config/gsm.mk)
+KANGOS_BUILDTYPE := OFFICIAL
+$(call inherit-product, vendor/kangos/config/common.mk)
 
 # Gapps
 USE_GAPPS := true
@@ -31,18 +41,22 @@ TARGET_SUPPORTS_STOCK_ARCORE := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := true
 
 # Define first api level
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Build Description
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="jasmine-user 9 PKQ1.180904.001 V10.0.9.0.PDIMIXM release-keys"
 
+# Build Fingerprint
+BUILD_FINGERPRINT := "xiaomi/wayne/wayne:8.1.0/OPM1.171019.011/V9.5.11.0.ODCCNFA:user/release-keys"
+
+# Density
+TARGET_SCREEN_DENSITY := 420
+
 # Device identifier
-PRODUCT_BRAND := Xiaomi
+PRODUCT_BRAND := xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := awaken_jasmine_sprout
+PRODUCT_NAME := kangos_jasmine_sprout
 PRODUCT_DEVICE := jasmine_sprout
 PRODUCT_MODEL := Mi A2
 
