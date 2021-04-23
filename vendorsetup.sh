aa04eab9aff23cd3389decedd9ded12921c9f8b4
nparashar150 <nparashar150@gmail.com>
2021-04-23 09:52:52 +0530

    Nuke DeviceDoze and DeviceSettings

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,5 @@
 # export
 export SKIP_ABI_CHECKS=true
 export BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
+rm -rf device/xiaomi/sdm660-common/DeviceDoze
+rm -rf device/xiaomi/sdm660-common/DeviceSettings
